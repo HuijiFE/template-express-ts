ba271898aee6e183286c95f697d181ebd1f29abe
duduluu <duduluu@outlook.com>
2018-03-21 16:16:06 +0800

    Remove useless code for CORS

--- src/server.ts
+++ src/server.ts
@@ -26,15 +26,6 @@ app
   .use(compression())
   .use(cors()) // CORS
   .use(logger('dev'))
-  .use((request, response, next) => {
-    // CORS 跨域
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  })
   .use('/', (request, response, next) =>
     response.send('Template Express on TypeScript.')
   );
